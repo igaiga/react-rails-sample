cb284f1bce0fcca09124fdf1335b0ba94f93cfb6
Kuniaki IGARASHI <igaiga@gmail.com>
2015-05-04 10:59:40 +0900

    Make dashboard#comments

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class DashboardController < ApplicationController
   def index
   end
+
+  def comments
+    data = [
+      {author: "igaiga", text: "This is a comment from rails"},
+      {author: "matz", text: "This is *another* comment from rails"}
+    ]
+    render json: data.to_json
+  end
 end
